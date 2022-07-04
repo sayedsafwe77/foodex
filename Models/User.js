f3553770a635388581d016fa9f8b6fe3848c7b5f
sayedsafwet77 <sayedsafwet4@gmail.com>
2022-07-04 12:26:55 +0200

    asdasd

--- Models/User.js
+++ Models/User.js
@@ -15,7 +15,7 @@ UserSchema.pre('save', async function() {
 })
 UserSchema.methods.generateAuthToken = async function() {
     let user = this;
-    let token = jwt.sign({ _id: user._id.toString() }, 'thisismysecretkey');
+    let token = jwt.sign({ _id: user._id.toString() }, process.env.JWT_SECRET_KEY);
     user.tokens.push({ token });
     await user.save();
     return token;
--- database/connection.js
+++ database/connection.js
@@ -3,7 +3,7 @@ const db_connect = () => {
     // mongoose.connect(`mongodb://${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`)
     //     .then(console.log('database connected'))
     //     .catch(err => console.log('error while connecting', err));
-    mongoose.connect('mongodb+srv://sayedsafwet:sayed771995@cluster0.rav7d.mongodb.net/food')
+    mongoose.connect(process.env.DATABASE_URL)
         .then(console.log('database connected'))
         .catch(err => console.log('error while connecting', err));
 }
