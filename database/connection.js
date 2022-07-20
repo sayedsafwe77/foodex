dd2cf1f8ce32f9e9c123142427dbe3508c8c5592
sayedsafwet77 <sayedsafwet4@gmail.com>
2022-07-20 17:15:30 +0200

    edit database connection string

--- database/connection.js
+++ database/connection.js
@@ -1,9 +1,9 @@
 let mongoose = require('mongoose');
 const db_connect = () => {
-    mongoose.connect(`mongodb://${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`)
-        //     .then(console.log('database connected'))
-        //     .catch(err => console.log('error while connecting', err));
-        // mongoose.connect('mongodb+srv://sayedsafwet:sayed771995@cluster0.rav7d.mongodb.net/food')
+    // mongoose.connect(`mongodb://${process.env.DATABASE_URL}/${process.env.DATABASE_NAME}`)
+    //     .then(console.log('database connected'))
+    //     .catch(err => console.log('error while connecting', err));
+    mongoose.connect('mongodb+srv://sayedsafwet:sayed771995@cluster0.rav7d.mongodb.net/food')
         .then(console.log('database connected'))
         .catch(err => console.log('error while connecting', err));
 }
